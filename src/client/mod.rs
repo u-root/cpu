20b4018fa4db0eeb094e7bb9226558863253dd91
Ronald G. Minnich <rminnich@gmail.com>
2022-03-08 11:42:17 -0800

    client/mod.rs: be a bit more tolerant of unset USER
    More to come
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -48,8 +48,16 @@ pub async fn ssh() {
   let key = thrussh_keys::key::KeyPair::generate_ed25519().unwrap();
   let mut agent = thrussh_keys::agent::client::AgentClient::connect_env().await.unwrap();
   agent.add_identity(&key, &[]).await.unwrap();
-  let mut session = thrussh::client::connect(config, "localhost:22", sh).await.unwrap();
-  if session.authenticate_future(std::env::var("USER").unwrap(), key.clone_public_key(), agent).await.1.unwrap() {
+  let mut session = thrussh::client::connect(config, "10.0.2.15:23", sh).await.unwrap();
+  let user:String;
+  match std::env::var("USER") {
+    Ok(val) => user = val,
+    Err(e) => {
+        user = "root".to_string();
+        println!("No USER set({}); going with {}", e, user);
+    },
+}
+  if session.authenticate_future(user, key.clone_public_key(), agent).await.1.unwrap() {
     let mut channel = session.channel_open_session().await.unwrap();
     channel.data(&b"Hello, world!"[..]).await.unwrap();
     if let Some(msg) = channel.wait().await {
