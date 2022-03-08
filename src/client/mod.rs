b91208e6c13cec86f653361c76dce2627a9e6069
Daniel Maslowski <info@orangecms.org>
2022-03-08 22:38:38 +0100

    Rewrite in Rust: paste server example from docs
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -151,3 +151,80 @@ pub async fn ssh() -> Result<()> {
     ssh.close().await?;
     Ok(())
 }
+
+/*
+#[derive(Clone)]
+struct Server {
+    client_pubkey: Arc<thrussh_keys::key::PublicKey>,
+    clients: Arc<Mutex<HashMap<(usize, ChannelId), thrussh::server::Handle>>>,
+    id: usize,
+}
+
+impl server::Server for Server {
+    type Handler = Self;
+    fn new(&mut self, _: Option<std::net::SocketAddr>) -> Self {
+        let s = self.clone();
+        self.id += 1;
+        s
+    }
+}
+
+impl server::Handler for Server {
+    type Error = anyhow::Error;
+    type FutureAuth = futures::future::Ready<Result<(Self, server::Auth), anyhow::Error>>;
+    type FutureUnit = futures::future::Ready<Result<(Self, Session), anyhow::Error>>;
+    type FutureBool = futures::future::Ready<Result<(Self, Session, bool), anyhow::Error>>;
+
+    fn finished_auth(mut self, auth: Auth) -> Self::FutureAuth {
+        futures::future::ready(Ok((self, auth)))
+    }
+    fn finished_bool(self, b: bool, s: Session) -> Self::FutureBool {
+        futures::future::ready(Ok((self, s, b)))
+    }
+    fn finished(self, s: Session) -> Self::FutureUnit {
+        futures::future::ready(Ok((self, s)))
+    }
+    fn channel_open_session(self, channel: ChannelId, session: Session) -> Self::FutureUnit {
+        {
+            let mut clients = self.clients.lock().unwrap();
+            clients.insert((self.id, channel), session.handle());
+        }
+        self.finished(session)
+    }
+    fn auth_publickey(self, _: &str, _: &key::PublicKey) -> Self::FutureAuth {
+        self.finished_auth(server::Auth::Accept)
+    }
+    fn data(self, channel: ChannelId, data: &[u8], mut session: Session) -> Self::FutureUnit {
+        {
+            let mut clients = self.clients.lock().unwrap();
+            for ((id, channel), ref mut s) in clients.iter_mut() {
+                if *id != self.id {
+                    s.data(*channel, CryptoVec::from_slice(data));
+                }
+            }
+        }
+        session.data(channel, CryptoVec::from_slice(data));
+        self.finished(session)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let client_key = thrussh_keys::key::KeyPair::generate_ed25519().unwrap();
+    let client_pubkey = Arc::new(client_key.clone_public_key());
+    let mut config = thrussh::server::Config::default();
+    config.connection_timeout = Some(std::time::Duration::from_secs(3));
+    config.auth_rejection_time = std::time::Duration::from_secs(3);
+    config.keys.push(thrussh_keys::key::KeyPair::generate_ed25519().unwrap());
+    let config = Arc::new(config);
+    let sh = Server{
+        client_pubkey,
+        clients: Arc::new(Mutex::new(HashMap::new())),
+        id: 0
+    };
+    tokio::time::timeout(
+       std::time::Duration::from_secs(1),
+       thrussh::server::run(config, "0.0.0.0:2222", sh)
+    ).await.unwrap_or(Ok(()));
+}
+*/
