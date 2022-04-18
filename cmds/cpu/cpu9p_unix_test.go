866156dac210b4c61691d6f52fb2efe35a22a735
Ronald G. Minnich <rminnich@gmail.com>
2022-04-18 14:05:28 -0700

    proposed table driven test for cpu9p_unix
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/cpu/cpu9p_unix_test.go
+++ cmds/cpu/cpu9p_unix_test.go
@@ -197,3 +197,146 @@ func Test9pUnix(t *testing.T) {
 	}
 
 }
+
+func Test9pUnixTable(t *testing.T) {
+	d := t.TempDir()
+	f := filepath.Join(d, "a")
+	if err := ioutil.WriteFile(f, []byte("hi"), 0666); err != nil {
+		t.Fatalf(`ioutil.WriteFile(%q, "hi", 0666): %v != nil`, f, err)
+	}
+	// First test: do nothing
+	m := p9.AttrMask{
+		Mode:        true,
+		NLink:       true,
+		UID:         true,
+		GID:         true,
+		RDev:        true,
+		ATime:       true,
+		MTime:       true,
+		CTime:       true,
+		INo:         true,
+		Size:        true,
+		Blocks:      true,
+		BTime:       true,
+		Gen:         true,
+		DataVersion: true,
+	}
+
+	c := &cpu9p{
+		path: f,
+	}
+
+	_, _, ga, err := c.GetAttr(m)
+	if err != nil {
+		t.Fatalf("Getattr: %v != nil", err)
+	}
+
+	sam := p9.SetAttrMask{
+		Permissions:        false,
+		UID:                false,
+		GID:                false,
+		Size:               false,
+		ATime:              false,
+		MTime:              false,
+		CTime:              false,
+		ATimeNotSystemTime: false,
+		MTimeNotSystemTime: false,
+	}
+	sa := p9.SetAttr{
+		Permissions:      0777,
+		UID:              ga.UID,
+		GID:              ga.GID,
+		Size:             ga.Size + 4,
+		ATimeSeconds:     ga.ATimeSeconds + 22,
+		ATimeNanoSeconds: ga.ATimeNanoSeconds + 33,
+		MTimeSeconds:     ga.MTimeSeconds + 55,
+		MTimeNanoSeconds: ga.MTimeNanoSeconds + 66,
+	}
+
+	var tests = []string{
+		"Change nothing",
+		"Setattr with Permissions",
+		"Setattr with size",
+		"Setattr with UID",
+		"Setattr with GID",
+		"Setattr with ATime to local system time",
+		"Setattr with MTime to local system time",
+		"Settattr with ATime to time in TSetattr",
+		"Settattr with MTime to time in TSetattr",
+	}
+	for i, msg := range tests {
+		switch i {
+		case 0:
+		case 1:
+			sam.Permissions = true
+		case 2:
+			sam.Size = true
+		case 3:
+			sam.UID = true
+		case 4:
+			sam.GID = true
+		case 5:
+			sam.ATime = true
+		case 6:
+			sam.MTime = true
+		case 7:
+			sam.ATimeNotSystemTime, sam.MTime = true, false
+		case 8:
+			sam.MTimeNotSystemTime, sam.ATime, sam.MTime = true, false, true
+		}
+
+		t.Logf("Test %v", msg)
+		if err := c.SetAttr(sam, sa); err != nil {
+			t.Fatalf(msg+":%v != nil", err)
+		}
+		_, _, gagot, err := c.GetAttr(m)
+		if err != nil {
+			t.Fatalf("Getattr: %v != nil", err)
+		}
+
+		switch i {
+		case 0:
+			if !reflect.DeepEqual(gagot, ga) {
+				t.Fatalf(msg+":%v != %v", gagot, ga)
+			}
+
+		case 1:
+			if gagot.Mode&p9.AllPermissions != sa.Permissions {
+				t.Fatalf("Permissions: %o != %o", gagot.Mode&p9.AllPermissions, sa.Permissions)
+			}
+		case 2:
+			if gagot.Size != sa.Size {
+				t.Fatalf("Size: %o != %o", gagot.Size, sa.Size)
+			}
+
+		case 3:
+			if gagot.UID != sa.UID {
+				t.Errorf("UID: %o != %o", gagot.UID, sa.UID)
+			}
+
+		case 4:
+			if gagot.GID != sa.GID {
+				t.Errorf("GID: %o != %o", gagot.GID, sa.GID)
+			}
+
+		case 5, 6:
+			// for setting ATime to "whatever the kernel says it is",
+			// there's no great test to see if it worked. Even testing
+			// to see it's not what it was can have weird errors if the
+			// file system you are on is NFS, or if while you are running
+			// the test NTP or sysadmins move time on you. We have to
+			// settle for "SetAttr got no error"
+		case 7:
+			if gagot.ATimeSeconds != sa.ATimeSeconds {
+				t.Errorf("Setting ATimeSeconds %d != %d", gagot.ATimeSeconds, sa.ATimeSeconds)
+			}
+		case 8:
+			if gagot.MTimeSeconds != sa.MTimeSeconds {
+				t.Errorf("Setting MTimeSeconds %d != %d", gagot.MTimeSeconds, sa.MTimeSeconds)
+			}
+
+		}
+
+	}
+
+}
