0de8d08e2107d3b2e758e77fe9829f29d2dd4528
Ronald G. Minnich <rminnich@gmail.com>
2022-09-30 18:20:31 +0000

    client: only pass CPU_FSTAB to cpud
    cpu has been able to pass an fstab in CPU_FSTAB for a while.
    
    This led to having two ways to specify a namespace to cpud:
    CPU_NAMESPACE and CPU_FSTAB.
    
    We can simplify this, and simplify cpud, by having the client
    parse the namespace and add it to CPU_FSTAB.
    
    Further, if the client parses the contents of -namespace
    for errors, feedback for bad values is much more
    immediate.
    
    This change:
    o implements parsing the namespace to CPU_FSTAB
    o concatenating it at the end of a user-specified
      fstab file (if any)
    o NOT passing CPU_NAMESPACE to cpud
    
    If this works, the next step is to remove CPU_NAMESPACE support
    from cpud, thereby simplifying it.
    
    The namespace switch will be preserved for the client, because
    it is very convenient.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- client/fns_test.go
+++ client/fns_test.go
@@ -34,3 +34,50 @@ func TestVsockIdPort(t *testing.T) {
 		}
 	}
 }
+
+func TestParseBinds(t *testing.T) {
+	for _, tt := range []struct {
+		namespace string
+		tmpmnt    string
+		fstab     string
+		err       error
+	}{
+		{"", "/tmp", "", nil},
+		{"/lib", "/tmpmnt", "/tmpmnt/cpu/lib /lib none defaults,bind 0 0\n", nil},
+		{"/lib=/arm/lib", "/tmpmnt", "/tmpmnt/cpu/arm/lib /lib none defaults,bind 0 0\n", nil},
+		{"/lib=/arm/lib:/bin", "/tmpmnt", "/tmpmnt/cpu/arm/lib /lib none defaults,bind 0 0\n/tmpmnt/cpu/bin /bin none defaults,bind 0 0\n", nil},
+		{"/lib=/arm/lib:/bin=/b/bin", "/tmpmnt", "/tmpmnt/cpu/arm/lib /lib none defaults,bind 0 0\n/tmpmnt/cpu/b/bin /bin none defaults,bind 0 0\n", nil},
+		{"/a:/b:/c:/d", "/t", "/t/cpu/a /a none defaults,bind 0 0\n/t/cpu/b /b none defaults,bind 0 0\n/t/cpu/c /c none defaults,bind 0 0\n/t/cpu/d /d none defaults,bind 0 0\n", nil},
+		{"/lib=:/bin=/b/bin", "/tmpmnt", "", strconv.ErrSyntax},
+		{"=/lib:/bin=/b/bin", "/tmpmnt", "", strconv.ErrSyntax},
+		{"/a::/bin=/b/bin", "/tmpmnt", "", strconv.ErrSyntax},
+		// Test the weird case that a bind name can contain an = sign.
+		// There is not imaginable case where we need this but ...
+		// note also that only remote names contain = signs pending
+		// more complex parsing. Perhaps it should not be allowed at all.
+		{"/a:/bin==/b/bin", "/tmpmnt", "/tmpmnt/cpu/a /a none defaults,bind 0 0\n/tmpmnt/cpu/=/b/bin /bin none defaults,bind 0 0\n", nil},
+	} {
+		f, err := parseBinds(tt.namespace, tt.tmpmnt)
+		if !errors.Is(err, tt.err) || f != tt.fstab {
+			t.Errorf("parseBinds(%q,%q): (%q,%v) != (%q,%v)", tt.namespace, tt.tmpmnt, f, err, tt.fstab, tt.err)
+		}
+	}
+}
+
+func TestJoinFSTab(t *testing.T) {
+	for _, tt := range []struct {
+		tables []string
+		fstab  string
+	}{
+		{tables: nil, fstab: ""},
+		{tables: []string{""}, fstab: "\n"},
+		{tables: []string{"a b c"}, fstab: "a b c\n"},
+		{tables: []string{"a b c\n\n", "d e f"}, fstab: "a b c\nd e f\n"},
+		{tables: []string{"a b c\n\n", "d e f"}, fstab: "a b c\nd e f\n"},
+		{tables: []string{"a b c\n\n", "d e f\n1 2 3"}, fstab: "a b c\nd e f\n1 2 3\n"},
+	} {
+		if fstab := joinFSTab(tt.tables...); fstab != tt.fstab {
+			t.Errorf("joinFSTab(%q): %q != %q", tt.tables, fstab, tt.fstab)
+		}
+	}
+}
