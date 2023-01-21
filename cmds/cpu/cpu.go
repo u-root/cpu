u-root/cpu

commit 2a649f85208bb01ccf4c4b9d54358b0924bc758f

cpu: forward SIGINT/SIGTERM to remote commands
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

cmds/cpu/cpu.go
@@ -12,8 +12,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
+	"syscall"
 
 	// We use this ssh because it implements port redirection.
 	// It can not, however, unpack password-protected keys yet.
@@ -129,9 +131,17 @@ func getPort(host, port string) string {
 	return p
 }
 
-func newCPU(host string, args ...string) error {
+func newCPU(host string, args ...string) (retErr error) {
 	// note that 9P is enabled if namespace is not empty OR if ninep is true
 	c := client.Command(host, args...)
+	defer func() {
+		verbose("close")
+		if err := c.Close(); err != nil && retErr == nil {
+			retErr = fmt.Errorf("Close: %v", err)
+		}
+		verbose("close done")
+	}()
+
 	if err := c.SetOptions(
 		client.WithPrivateKeyFile(*keyFile),
 		client.WithHostKeyFile(*hostKeyFile),
@@ -149,17 +159,46 @@ func newCPU(host string, args ...string) error {
 	if err := c.Dial(); err != nil {
 		return fmt.Errorf("Dial: %v", err)
 	}
-	verbose("start")
-	if err := c.Start(); err != nil {
-		return fmt.Errorf("Start: %v", err)
+
+	sigChan := make(chan os.Signal, 1)
+	defer close(sigChan)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigChan)
+	errChan := make(chan error, 1)
+	defer close(errChan)
+
+	go func() {
+		verbose("start")
+		if err := c.Start(); err != nil {
+			errChan <- fmt.Errorf("Start: %v", err)
+			return
+		}
+		verbose("wait")
+		errChan <- c.Wait()
+	}()
+
+	var err error
+loop:
+	for {
+		select {
+		case sig := <-sigChan:
+			var sigErr error
+			switch sig {
+			case syscall.SIGINT:
+				sigErr = c.Signal(ossh.SIGINT)
+			case syscall.SIGTERM:
+				sigErr = c.Signal(ossh.SIGTERM)
+			}
+			if sigErr != nil {
+				verbose("sending %v to %q: %v", sig, c.Args[0], sigErr)
+			} else {
+				verbose("signal %v sent to %q", sig, c.Args[0])
+			}
+		case err = <-errChan:
+			break loop
+		}
 	}
-	verbose("wait")
-	if err := c.Wait(); err != nil {
-		log.Printf("Wait: %v\r\n", err)
-	}
-	verbose("close")
-	err := c.Close()
-	verbose("close done")
+
 	return err
 }
 

server/server.go
@@ -62,6 +62,27 @@ func handler(s ssh.Session) {
 	a := s.Command()
 	verbose("handler: cmd is %q", a)
 	cmd := command(a[0], a[1:]...)
+
+	sigChan := make(chan ssh.Signal, 1)
+	defer close(sigChan)
+	s.Signals(sigChan)
+	go func() {
+		for signal := range sigChan {
+			var err error
+			switch signal {
+			case ssh.SIGTERM:
+				err = cmd.Process.Signal(syscall.SIGTERM)
+			case ssh.SIGINT:
+				err = cmd.Process.Signal(syscall.SIGINT)
+			default:
+				err = fmt.Errorf("unknown signal: %q", signal)
+			}
+			if err != nil {
+				verbose("sending %q to %q: %v", signal, a[0], err)
+			}
+		}
+	}()
+
 	cmd.Env = append(cmd.Env, s.Environ()...)
 	ptyReq, winCh, isPty := s.Pty()
 	if isPty {

commit 832659c90fff32aa047bf2c9b3d7ab771b136675

session: forward SIGINT/SIGTERM to sub commands
Fixes: 2a649f85208b ("cpu: forward SIGINT/SIGTERM to remote commands")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

cmds/cpu/cpu.go
@@ -15,7 +15,6 @@ import (
 	"os/signal"
 	"path/filepath"
 	"strings"
-	"syscall"
 
 	// We use this ssh because it implements port redirection.
 	// It can not, however, unpack password-protected keys yet.
@@ -26,6 +25,7 @@ import (
 
 	// We use this ssh because it can unpack password-protected private keys.
 	ossh "golang.org/x/crypto/ssh"
+	"golang.org/x/sys/unix"
 )
 
 const defaultPort = "17010"
@@ -162,7 +162,7 @@ func newCPU(host string, args ...string) (retErr error) {
 
 	sigChan := make(chan os.Signal, 1)
 	defer close(sigChan)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigChan, unix.SIGINT, unix.SIGTERM)
 	defer signal.Stop(sigChan)
 	errChan := make(chan error, 1)
 	defer close(errChan)
@@ -184,9 +184,9 @@ loop:
 		case sig := <-sigChan:
 			var sigErr error
 			switch sig {
-			case syscall.SIGINT:
+			case unix.SIGINT:
 				sigErr = c.Signal(ossh.SIGINT)
-			case syscall.SIGTERM:
+			case unix.SIGTERM:
 				sigErr = c.Signal(ossh.SIGTERM)
 			}
 			if sigErr != nil {

server/server.go
@@ -18,6 +18,7 @@ import (
 	// It can not, however, unpack password-protected keys yet.
 	"github.com/creack/pty"
 	"github.com/gliderlabs/ssh"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -71,9 +72,9 @@ func handler(s ssh.Session) {
 			var err error
 			switch signal {
 			case ssh.SIGTERM:
-				err = cmd.Process.Signal(syscall.SIGTERM)
+				err = cmd.Process.Signal(unix.SIGTERM)
 			case ssh.SIGINT:
-				err = cmd.Process.Signal(syscall.SIGINT)
+				err = cmd.Process.Signal(unix.SIGINT)
 			default:
 				err = fmt.Errorf("unknown signal: %q", signal)
 			}

session/fns.go
@@ -6,11 +6,43 @@ package session
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
 	"strings"
+
+	"golang.org/x/sys/unix"
 )
 
 func verbose(f string, a ...interface{}) {
 	v("session:"+f, a...)
+}
+
+func runCmd(c *exec.Cmd) error {
+	sigChan := make(chan os.Signal, 1)
+	defer close(sigChan)
+	signal.Notify(sigChan, unix.SIGTERM, unix.SIGINT)
+	defer signal.Stop(sigChan)
+	errChan := make(chan error, 1)
+	defer close(errChan)
+	go func() {
+		errChan <- c.Run()
+	}()
+	var err error
+loop:
+	for {
+		select {
+		case sig := <-sigChan:
+			if sigErr := c.Process.Signal(sig); sigErr != nil {
+				verbose("sending %v to %q: %v", sig, c.Args[0], sigErr)
+			} else {
+				verbose("signal %v sent to %q", sig, c.Args[0])
+			}
+		case err = <-errChan:
+			break loop
+		}
+	}
+	return err
 }
 
 // ParseBinds parses a CPU_NAMESPACE-style string to a

session/session.go
@@ -221,7 +221,7 @@ func (s *Session) Run() error {
 		log.Printf("CPUD: your $PWD %s is not in the remote namespace", c.Dir)
 		return os.ErrNotExist
 	}
-	err = c.Run()
+	err = runCmd(c)
 	verbose("Run %v returns %v", c, err)
 	if err != nil {
 		if s.fail && len(wtf) != 0 {