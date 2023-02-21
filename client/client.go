u-root/cpu

commit aa39f631c1887010310ab7288cb7192938191ce6

client.go: Support Firecracker virtio-vsock
Add a new network type `unix-vsock` and implement the
Firecracker virtio-vsock model:
https://github.com/firecracker-microvm/firecracker/blob/main/docs/vsock.md#host-initiated-connections

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

client/client.go
@@ -5,6 +5,7 @@
 package client
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -293,6 +294,23 @@ func vsockDial(host, port string) (net.Conn, string, error) {
 
 }
 
+// https://github.com/firecracker-microvm/firecracker/blob/main/docs/vsock.md#host-initiated-connections
+func unixVsockDial(path, port string) (net.Conn, string, error) {
+	conn, err := net.Dial("unix", path)
+	if err != nil {
+		return nil, "", err
+	}
+	connectMsg := []byte(fmt.Sprintf("CONNECT %s\n", port))
+	if n, err := conn.Write(connectMsg); err != nil || n != len(connectMsg) {
+		V("send connect request err, number of sent bytes = %d: %v", n, err)
+	}
+	s := bufio.NewScanner(conn)
+	if !s.Scan() || !strings.HasPrefix(s.Text(), "OK") {
+		V("connect request failed.")
+	}
+	return conn, path, nil
+}
+
 // Dial implements ssh.Dial for cpu.
 // Additionaly, if Cmd.Root is not "", it
 // starts up a server for 9p requests.
@@ -320,8 +338,10 @@ func (c *Cmd) Dial() error {
 		conn, addr, err = vsockDial(c.HostName, c.Port)
 	case "unix", "unixgram", "unixpacket":
 		// There is not port on a unix domain socket.
-		addr = c.network
-		conn, err = net.Dial(c.network, c.Port)
+		addr = c.HostName
+		conn, err = net.Dial(c.network, c.HostName)
+	case "unix-vsock":
+		conn, addr, err = unixVsockDial(c.HostName, c.Port)
 	default:
 		addr = net.JoinHostPort(c.HostName, c.Port)
 		conn, err = net.Dial(c.network, addr)

commit 7552d0f3a1cb98cdcd298aeafc4c51fc037a0a92

client: fix ssh handshake failure caused by vsock
In the Cloud Hypervisor vsock model, after the VMM accepted the vsock
connection request from a host application, the VMM establishes the
connection to a guest `accept()` call and sends `OK <port>\n` to the
host application. When the host application reads from the socket,
the socket buffer might contain `OK <port>\n' as well as some bytes
from the application in the guest.

In the original unixVsockDial() implementation, `bufio.NewScanner`
could cause error `ssh: overflow reading version string`, because once
the guest ssh server accepts a request, it immediately starts
handshaking. On the other hand, the host application might just have
started verifying `OK <port>\n'. `bufio.NewScanner` might read
`OK <port>\n' as well as some ssh handshaking bytes from the socket.
These bytes are lost from the point of view of following client ssh
handshaking calls.

Fixes: aa39f631c188 ("client.go: Support Firecracker virtio-vsock")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

client/client.go
@@ -5,7 +5,6 @@
 package client
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -296,20 +295,29 @@ func vsockDial(host, port string) (net.Conn, string, error) {
 }
 
 // https://github.com/firecracker-microvm/firecracker/blob/main/docs/vsock.md#host-initiated-connections
-func unixVsockDial(path, port string) (net.Conn, string, error) {
+func unixVsockDial(path, port string) (net.Conn, error) {
 	conn, err := net.Dial("unix", path)
 	if err != nil {
-		return nil, "", err
+		return nil, err
 	}
-	connectMsg := []byte(fmt.Sprintf("CONNECT %s\n", port))
-	if n, err := conn.Write(connectMsg); err != nil || n != len(connectMsg) {
-		verbose("send connect request err, number of sent bytes = %d: %v", n, err)
+	connectMsg := fmt.Sprintf("CONNECT %s\n", port)
+	if _, err := io.WriteString(conn, connectMsg); err != nil {
+		return nil, fmt.Errorf("sending connect request: %w", err)
 	}
-	s := bufio.NewScanner(conn)
-	if !s.Scan() || !strings.HasPrefix(s.Text(), "OK") {
-		verbose("connect request failed.")
+	buf := make([]byte, 2)
+	if _, err := io.ReadFull(conn, buf); err != nil {
+		return nil, fmt.Errorf("reading connect request: %w", err)
 	}
-	return conn, path, nil
+	if string(buf) != "OK" {
+		return nil, fmt.Errorf("vsock: expect OK, got %s", buf)
+	}
+	buf = make([]byte, 1)
+	for buf[0] != '\n' {
+		if _, err := io.ReadFull(conn, buf); err != nil {
+			return nil, err
+		}
+	}
+	return conn, nil
 }
 
 // Dial implements ssh.Dial for cpu.
@@ -342,7 +350,8 @@ func (c *Cmd) Dial() error {
 		addr = c.HostName
 		conn, err = net.Dial(c.network, c.HostName)
 	case "unix-vsock":
-		conn, addr, err = unixVsockDial(c.HostName, c.Port)
+		addr = c.HostName
+		conn, err = unixVsockDial(c.HostName, c.Port)
 	default:
 		addr = net.JoinHostPort(c.HostName, c.Port)
 		conn, err = net.Dial(c.network, addr)