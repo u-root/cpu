1722d19d4c67493a79554f1a82fa000e530cbc76
Changyuan Lyu <changyuanl@google.com>
2023-04-22 21:57:34 -0700

    p9cpu: implement rpc server methods
    Add a new dummy Server struct which will implement the actual
    cpu server logic.
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- p9cpu/crates/libp9cpu/src/rpc/rpc_server.rs
+++ p9cpu/crates/libp9cpu/src/rpc/rpc_server.rs
@@ -1,14 +1,17 @@
+use super::{
+    Bytes, Code, Empty, NinepForwardRequest, PrependedStream, SessionId, StartRequest, StdinRequest,
+};
 use crate::rpc;
+use crate::server;
 use crate::Addr;
 use async_trait::async_trait;
-use futures::stream::Stream;
+use futures::stream::{Stream, StreamExt};
 use std::pin::Pin;
 use thiserror::Error;
-use tonic::transport::Server;
 use tonic::{Request, Response, Status, Streaming};
 
 #[derive(Error, Debug)]
-pub enum RpcServerError {
+pub enum Error {
     #[error("RPC error: {0}")]
     Transport(#[from] tonic::transport::Error),
     #[error("IO error: {0}")]
@@ -19,11 +22,10 @@ pub struct RpcServer {}
 
 #[async_trait]
 impl crate::server::P9cpuServerT for RpcServer {
-    type Error = RpcServerError;
-    async fn serve(&self, addr: Addr) -> Result<(), RpcServerError> {
-        
+    type Error = Error;
+    async fn serve(&self, addr: Addr) -> Result<(), Error> {
         let p9cpu_service = rpc::p9cpu_server::P9cpuServer::new(P9cpuService::default());
-        let router = Server::builder().add_service(p9cpu_service);
+        let router = tonic::transport::Server::builder().add_service(p9cpu_service);
         match addr {
             Addr::Tcp(addr) => router.serve(addr).await?,
             Addr::Uds(_addr) => {
@@ -39,8 +41,20 @@ impl crate::server::P9cpuServerT for RpcServer {
 
 type RpcResult<T> = Result<Response<T>, Status>;
 
+impl From<server::Error> for Status {
+    fn from(e: server::Error) -> Self {
+        tonic::Status::internal(e.to_string())
+    }
+}
+
+fn vec_to_uuid(v: &Vec<u8>) -> Result<uuid::Uuid, Status> {
+    uuid::Uuid::from_slice(v).map_err(|e| Status::invalid_argument(e.to_string()))
+}
+
 #[derive(Debug, Default)]
-pub struct P9cpuService {}
+pub struct P9cpuService {
+    server: server::Server<uuid::Uuid>,
+}
 
 #[async_trait]
 impl rpc::p9cpu_server::P9cpu for P9cpuService {
@@ -48,37 +62,86 @@ impl rpc::p9cpu_server::P9cpu for P9cpuService {
     type StderrStream = Pin<Box<dyn Stream<Item = Result<rpc::Bytes, Status>> + Send>>;
     type NinepForwardStream = Pin<Box<dyn Stream<Item = Result<rpc::Bytes, Status>> + Send>>;
 
-    async fn start(&self, _request: Request<rpc::StartRequest>) -> RpcResult<rpc::Empty> {
-        unimplemented!()
+    async fn start(&self, request: Request<rpc::StartRequest>) -> RpcResult<rpc::Empty> {
+        let StartRequest { id, cmd: Some(cmd) } = request.into_inner() else {
+            return Err(Status::invalid_argument("No cmd provided."));
+        };
+        let sid = vec_to_uuid(&id)?;
+        // let Some(cmd) = request.
+        self.server.start(cmd, sid).await?;
+        Ok(Response::new(Empty {}))
     }
 
-    async fn stdin(
-        &self,
-        _request: Request<Streaming<rpc::StdinRequest>>,
-    ) -> RpcResult<rpc::Empty> {
-        unimplemented!()
+    async fn stdin(&self, request: Request<Streaming<rpc::StdinRequest>>) -> RpcResult<rpc::Empty> {
+        let mut in_stream = request.into_inner();
+        let Some(Ok(StdinRequest { id: Some(id), data })) = in_stream.next().await else {
+            return Err(Status::invalid_argument("no session id."));
+        };
+        let sid = vec_to_uuid(&id)?;
+        let byte_stream = in_stream.scan((), |_s, req| match req {
+            Ok(r) => futures::future::ready(Some(r.data)),
+            Err(e) => {
+                log::error!("Session {} stdin stream error: {:?}", sid, e);
+                futures::future::ready(None)
+            }
+        });
+        let stream = PrependedStream {
+            stream: byte_stream,
+            item: Some(data),
+        };
+        self.server.stdin(&sid, stream).await?;
+        Ok(Response::new(Empty {}))
     }
 
-    async fn stdout(&self, _request: Request<rpc::SessionId>) -> RpcResult<Self::StdoutStream> {
-        unimplemented!()
+    async fn stdout(&self, request: Request<rpc::SessionId>) -> RpcResult<Self::StdoutStream> {
+        let sid = vec_to_uuid(&request.into_inner().id)?;
+        let stream = self.server.stdout(&sid).await?;
+        let out_stream = stream.map(|data| Ok(Bytes { data }));
+        Ok(Response::new(Box::pin(out_stream) as Self::StdoutStream))
     }
 
-    async fn stderr(&self, _request: Request<rpc::SessionId>) -> RpcResult<Self::StderrStream> {
-        unimplemented!()
+    async fn stderr(&self, request: Request<rpc::SessionId>) -> RpcResult<Self::StderrStream> {
+        let sid = vec_to_uuid(&request.into_inner().id)?;
+        let stream = self.server.stderr(&sid).await?;
+        let err_stream = stream.map(|data| Ok(Bytes { data }));
+        Ok(Response::new(Box::pin(err_stream) as Self::StderrStream))
     }
 
     async fn dial(&self, _: Request<rpc::Empty>) -> RpcResult<rpc::SessionId> {
-        unimplemented!()
+        let sid = uuid::Uuid::new_v4();
+        self.server.dial(sid).await?;
+        let r = SessionId {
+            id: sid.into_bytes().into(),
+        };
+        Ok(Response::new(r))
     }
 
     async fn ninep_forward(
         &self,
-        _request: Request<Streaming<rpc::NinepForwardRequest>>,
+        request: Request<Streaming<rpc::NinepForwardRequest>>,
     ) -> RpcResult<Self::NinepForwardStream> {
-        unimplemented!()
+        let mut in_stream = request.into_inner();
+        let Some(Ok(NinepForwardRequest { id: Some(id), data: _ })) = in_stream.next().await else {
+            return Err(Status::invalid_argument("no session id."));
+        };
+        let sid = vec_to_uuid(&id)?;
+        let byte_stream = in_stream.scan((), move |_s, req| match req {
+            Ok(r) => futures::future::ready(Some(r.data)),
+            Err(e) => {
+                log::error!("Session {} stdin stream error: {:?}", sid, e);
+                futures::future::ready(None)
+            }
+        });
+        let out_stream = self.server.ninep_forward(&sid, byte_stream).await?;
+        let result_stream = out_stream.map(|data| Ok(Bytes { data }));
+        Ok(Response::new(
+            Box::pin(result_stream) as Self::NinepForwardStream
+        ))
     }
 
-    async fn wait(&self, _request: Request<rpc::SessionId>) -> RpcResult<rpc::Code> {
-        unimplemented!()
+    async fn wait(&self, request: Request<rpc::SessionId>) -> RpcResult<rpc::Code> {
+        let sid = vec_to_uuid(&request.into_inner().id)?;
+        let code = self.server.wait(&sid).await?;
+        Ok(Response::new(Code { code }))
     }
 }
--- p9cpu/crates/libp9cpu/src/server.rs
+++ p9cpu/crates/libp9cpu/src/server.rs
@@ -1,7 +1,73 @@
+use crate::cmd;
 use async_trait::async_trait;
+use futures::Stream;
+use thiserror::Error;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
 
 #[async_trait]
 pub trait P9cpuServerT {
     type Error: std::error::Error;
     async fn serve(&self, addr: crate::Addr) -> Result<(), Self::Error>;
+}
+
+#[derive(Error, Debug)]
+pub enum Error {}
+
+#[derive(Debug)]
+pub struct Server<I> {
+    _phantom: Option<I>,
+}
+
+impl<I> Default for Server<I> {
+    fn default() -> Self {
+        Self { _phantom: None }
+    }
+}
+
+impl<I> Server<I> {
+    pub async fn start(&self, _command: cmd::CommandReq, _sid: I) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    pub async fn stdin(
+        &self,
+        _sid: &I,
+        mut _in_stream: impl Stream<Item = Vec<u8>> + Unpin,
+    ) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    pub async fn stdout(&self, _sid: &I) -> Result<impl Stream<Item = Vec<u8>>, Error> {
+        // Not implemented yet
+        let (_tx, rx) = mpsc::channel(10);
+        let stream = ReceiverStream::new(rx);
+        Ok(stream)
+    }
+
+    pub async fn stderr(&self, _sid: &I) -> Result<impl Stream<Item = Vec<u8>>, Error> {
+        // Not implemented yet
+        let (_tx, rx) = mpsc::channel(10);
+        let stream = ReceiverStream::new(rx);
+        Ok(stream)
+    }
+
+    pub async fn dial(&self, _sid: I) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    pub async fn wait(&self, _sid: &I) -> Result<i32, Error> {
+        unimplemented!()
+    }
+
+    pub async fn ninep_forward(
+        &self,
+        _sid: &I,
+        mut _in_stream: impl Stream<Item = Vec<u8>> + Unpin + Send + 'static,
+    ) -> Result<impl Stream<Item = Vec<u8>>, Error> {
+        // Not implemented yet
+        let (_tx, rx) = mpsc::channel(10);
+        let stream = ReceiverStream::new(rx);
+        Ok(stream)
+    }
 }
