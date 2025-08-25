25153e0db38c71845b5a3b71c921a0ff6059139b
Ronald G Minnich <rminnich@gmail.com>
2025-08-25 15:14:44 -0700

    add the initramfs package
    The initramfs package contains a map[string][]byte.
    
    It includes compressed kernel and initramfs for several kernel and
    architecture combinations, as well as qemu command line.
    
    It is created by go generate.
    
    The package uses embed to incorporate those files. They are reasonably
    small.
    
    To see a possible usage, read vm_test.go
    
    set up vm_test to direct all output to a pipe
    
    Sadly, the original sin of os.Std{in,out,err} requires
    an os.File, else we could use an in-memory pipe.
    
    Use CombinedOutput in vm_test.
    
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

---
+++ initramfs/initramfs.go
@@ -0,0 +1,176 @@
+//go:generate go run build.go
+
+package initramfs
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"context"
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/u-root/cpu/client"
+)
+
+// generate this boiler plate code? Not clear it's worth it,
+// it changes maybe once a year.
+
+//go:embed initramfs_linux_amd64.cpio.gz
+var linux_amd64 []byte
+
+//go:embed initramfs_linux_arm64.cpio.gz
+var linux_arm64 []byte
+
+//go:embed initramfs_linux_arm.cpio.gz
+var linux_arm []byte
+
+//go:embed initramfs_linux_riscv64.cpio.gz
+var linux_riscv64 []byte
+
+//go:embed kernel_linux_amd64.gz
+var kernel_linux_amd64 []byte
+
+//go:embed kernel_linux_arm64.gz
+var kernel_linux_arm64 []byte
+
+//go:embed kernel_linux_arm.gz
+var kernel_linux_arm []byte
+
+//go:embed kernel_linux_riscv64.gz
+var kernel_linux_riscv64 []byte
+
+type Image struct {
+	Kernel    []byte
+	InitRAMFS []byte
+	Cmd       []string
+	Env       []string
+	dir       string
+}
+
+var images = map[string]Image{
+	"linux_amd64":   Image{Kernel: kernel_linux_amd64, InitRAMFS: linux_amd64, Cmd: []string{"qemu-system-x86_64", "-m", "1G"}},
+	"linux_arm64":   Image{Kernel: kernel_linux_arm64, InitRAMFS: linux_arm64, Cmd: []string{"qemu-system-aarch64", "-machine", "virt", "-cpu", "max", "-m", "1G"}},
+	"linux_arm":     Image{Kernel: kernel_linux_arm, InitRAMFS: linux_arm, Cmd: []string{"qemu-system-arm", "-M", "virt,highmem=off"}},
+	"linux_riscv64": Image{Kernel: kernel_linux_riscv64, InitRAMFS: linux_riscv64, Cmd: []string{"qemu-system-riscv64", "-M", "virt", "-cpu", "rv64", "-m", "1G"}},
+}
+
+func New(kernel, arch string) (*Image, error) {
+	common := []string{ /*"-serial", "/dev/tty",*/ "-nographic",
+		"-netdev", "user,id=net0,ipv4=on,hostfwd=tcp::17010-:17010",
+		// required for mac. No idea why. Should work on linux. If not, we'll need a bit
+		// more logic.
+		"-device", "e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27",
+		// No password needed, you're just a guest vm ...
+		// The kernel may not understand ip=dhcp, in which case it just ends up
+		// in init's environment.
+		//"--append", "ip=dhcp init=/init -pk \"\" -d",
+		"--append", "ip=dhcp init=/init -pk \"\"",
+	}
+	env := []string{}
+	n := fmt.Sprintf("%s_%s", kernel, arch)
+	im, ok := images[n]
+	if !ok {
+		return nil, fmt.Errorf("(%q,%q): %w", kernel, arch, os.ErrNotExist)
+	}
+
+	r, err := gzip.NewReader(bufio.NewReader(bytes.NewBuffer(im.InitRAMFS)))
+	if err != nil {
+		return nil, err
+	}
+
+	i, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("unzipped %d bytes: %w", len(i), err)
+	}
+
+	if r, err = gzip.NewReader(bufio.NewReader(bytes.NewBuffer(im.Kernel))); err != nil {
+		return nil, err
+	}
+
+	k, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("unzipped %d bytes: %w", len(k), err)
+	}
+	return &Image{Kernel: k, InitRAMFS: i, Cmd: append(im.Cmd, common...), Env: append(env, im.Env...)}, nil
+}
+
+func Uroot(d string) (string, error) {
+	c := exec.Command("u-root", "-o", filepath.Join(d, "uroot.cpio"))
+	c.Env = append(os.Environ(), "CGO_ENABLED=0")
+	if out, err := c.CombinedOutput(); err != nil {
+		return "", fmt.Errorf("u-root initramfs:%q:%w", out, err)
+	}
+	return filepath.Join(d, "uroot.cpio"), nil
+
+}
+
+func (image *Image) CommandContext(ctx context.Context, d string, extra ...string) (*exec.Cmd, error) {
+	image.dir = d
+	i, k := filepath.Join(d, "initramfs"), filepath.Join(d, "kernel")
+
+	if err := os.WriteFile(k, image.Kernel, 0644); err != nil {
+		return nil, err
+	}
+
+	ir, err := os.OpenFile(i, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	defer ir.Close()
+	for _, n := range extra {
+		f, err := os.Open(n)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+		if _, err := io.Copy(ir, f); err != nil {
+			return nil, err
+		}
+	}
+	if _, err := ir.Write(image.InitRAMFS); err != nil {
+		return nil, err
+	}
+
+	ir.Close()
+	c := exec.CommandContext(ctx, image.Cmd[0], append(image.Cmd[1:], "-kernel", k, "-initrd", i)...)
+	c.Env = append(os.Environ(), c.Env...)
+	c.Dir = d
+	return c, nil
+}
+
+func (*Image) StartVM(c *exec.Cmd) error {
+	if err := c.Start(); err != nil {
+		return fmt.Errorf("starting VM: %w", err)
+	}
+	time.Sleep(time.Second)
+	return nil
+}
+
+func (i *Image) CPUCommand(arg string, args ...string) (*client.Cmd, error) {
+	cpu := client.Command("127.0.0.1", append([]string{arg}, args...)...)
+	cpu.Env = os.Environ()
+
+	if err := cpu.SetOptions(
+		client.WithDisablePrivateKey(true),
+		client.WithPort("17010"),
+		client.WithRoot(i.dir),
+		client.WithNameSpace("/"),
+		client.With9P(true),
+		client.WithTimeout("5s"),
+	); err != nil {
+		return nil, err
+	}
+
+	if err := cpu.Dial(); err != nil {
+		return nil, err
+	}
+	cpu.Env = append(cpu.Env, "PWD=/")
+	return cpu, nil
+}
